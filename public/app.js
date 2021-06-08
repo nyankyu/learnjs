ef071484ab14088f3db843715e3fbc4016c4764d
nohtou <nohtou@student.42tokyo.jp>
2021-06-08 16:57:21 +0900

    add template

--- public/app.js
+++ public/app.js
@@ -3,8 +3,9 @@
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Comming soon!';
-  return $('<div class="problem-view">').text(title);
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text('Problem #' + problemNumber + ' Comming soon!');
+  return view;
 };
 
 learnjs.showView = function(hash) {
