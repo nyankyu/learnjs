5898987b429f4e1ea754241504bb65c3ac592e73
nohtou <nohtou@student.42tokyo.jp>
2021-06-09 23:07:49 +0900

    create app shell

--- public/app.js
+++ public/app.js
@@ -31,6 +31,10 @@ learnjs.buildCorrectFlash = function(problemNumber) {
   return correctFlash;
 };
 
+learnjs.landingView = function() {
+  return learnjs.template('landing-view');
+};
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
@@ -61,7 +65,9 @@ learnjs.problemView = function(data) {
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem': learnjs.problemView
+    '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
+    '': learnjs.landingView,
   };
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
